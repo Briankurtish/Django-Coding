e1e63ed76b495ae4e1d3645abeb7ce3c2dbcf907
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-03-04 00:49:30 +0300

    created the logout function

--- GithubProfileImage/img/views.py
+++ GithubProfileImage/img/views.py
@@ -61,3 +61,8 @@ def login(request):
             return redirect('login')
     else:
         return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
