69fee4e5ca95b1f2aaa4541373bd13d54b06c0dc
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-03-04 00:54:40 +0300

    created githubuser model

--- GithubProfileImage/img/models.py
+++ GithubProfileImage/img/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Github(models.Model):
+    githubuser = models.CharField(max_length=1000),
+    imagelink = models.CharField(max_length=10000),
+    username = models.CharField(max_length=1000),
+
+    def __str__(self):
+        return self.githubuser
